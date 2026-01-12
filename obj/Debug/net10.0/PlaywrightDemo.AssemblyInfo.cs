699ea6e139143f7e961bde3be6a2b95e230f1bc1
Haris George <harisgeorge97@gmail.com>
2026-01-12 17:28:53 +1100

    Initial commit - Playwright SnipeIT login solution

--- obj/Debug/net10.0/PlaywrightDemo.AssemblyInfo.cs
+++ obj/Debug/net10.0/PlaywrightDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b584a09a201e6140af07171b6ee2d2da85369f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
